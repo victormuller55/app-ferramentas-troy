ea295619b40c8d1063a0bbb3610a00c62de24f55
Victor Muller <victormuller050@gmail.com>
2024-02-27 14:27:12 -0300

    m

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-import network_info_plus_macos
-import path_provider_macos
-import share_plus_macos
-import shared_preferences_macos
-import url_launcher_macos
+import network_info_plus
+import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
